6272fa65498f26dae0d74add2cd12e5fe3427ef2
lndmfnlgs <izinovyev42@gmail.com>
2020-12-27 20:15:58 +0300

    refactoring

--- app/src/main/java/com/google/samples/apps/sunflower/data/GardenPlantingDao.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/data/GardenPlantingDao.kt
@@ -16,11 +16,7 @@
 
 package com.google.samples.apps.sunflower.data
 
-import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.Query
-import androidx.room.Transaction
+import androidx.room.*
 import kotlinx.coroutines.flow.Flow
 
 /**
